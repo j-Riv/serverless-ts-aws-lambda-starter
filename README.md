79ed34f615cba81e8eb27a7e7bf317ac6d2deb3a
j-Riv <jose.alfredo.rivera@gmail.com>
2021-10-09 21:07:17 -0700

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Serverless AWS Starter
+
+> Serverless + TypeScript starter for AWS Lambda using API Gateway
+
+### Setup
+
+```bash
+SECRET_KEY=RANDOM_STRING
+```
+
+### Resources
+
+- [Serverless Reference](https://www.serverless.com/framework/docs/providers/aws/guide/serverless.yml)
---
+++ src/functions/helloFriend.ts
@@ -0,0 +1,20 @@
+import {
+  APIGatewayProxyHandler,
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult
+} from 'aws-lambda';
+
+export const handler: APIGatewayProxyHandler = async (_event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    const response = {
+      statusCode: 200,
+      body: 'Hello, friend...',
+    };
+    return response;
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: 'An error occured',
+    };
+  }
+};
